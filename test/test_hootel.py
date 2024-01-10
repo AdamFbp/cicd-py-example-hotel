56b232a0973ea3f6be4b5fc9157fc8d567d30ac6
AdamFbp <142731015+AdamFbp@users.noreply.github.com>
2024-01-10 19:44:22 +0100

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -4,6 +4,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import allure
 import pytest
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class TestHootel(object):
